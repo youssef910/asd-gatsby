2b13e2c2049c0c691c0614b1cbd694d4bd53d50f
youssef910 <youssef.sadek910@gmail.com>
2022-02-02 23:54:19 +0000

    add what we do

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,12 +3,14 @@ import Welcome from "./welcome/welcome"
 import Layout from "../Components/Layout"
 import ScrollDown from "./welcome/scrollDown"
 import AboutUs from "./welcome/aboutUs"
+import WhatWeDo from "./welcome/whatWeDo"
 export default function Home() {
   return (
     <Layout>
       <Welcome />
       <ScrollDown />
       <AboutUs />
+      <WhatWeDo />
     </Layout>
   )
 }
