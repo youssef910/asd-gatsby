5f5fd30c38b82106dfc0890c112f65620ee4baae
youssef910 <youssef.sadek910@gmail.com>
2022-02-02 23:31:52 +0000

    create register blank page

---
+++ src/pages/register.js
@@ -0,0 +1,10 @@
+import React from "react"
+import Layout from "../Components/Layout"
+
+export default function register() {
+  return (
+    <Layout>
+      <div>register</div>
+    </Layout>
+  )
+}
