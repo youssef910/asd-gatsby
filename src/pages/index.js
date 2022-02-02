57f8b6461c9258c9d5b315b8b42a133418b813b2
youssef910 <youssef.sadek910@gmail.com>
2022-02-02 23:31:22 +0000

    Add about to home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,11 +2,13 @@ import React from "react"
 import Welcome from "./welcome/welcome"
 import Layout from "../Components/Layout"
 import ScrollDown from "./welcome/scrollDown"
+import AboutUs from "./welcome/aboutUs"
 export default function Home() {
   return (
     <Layout>
       <Welcome />
       <ScrollDown />
+      <AboutUs />
     </Layout>
   )
 }
